b91b27b32a9e29a9f3a96d625c04b5151e20a53b
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-07-30 11:38:24 -0700

    add quiz_app

---
+++ quiz_app/main.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+let api = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
+let decision;
+async function getData() {
+    let quiz = await fetch(api);
+    let result = await quiz.json();
+    return result.results;
+}
+let data = await getData();
+async function welcome() {
+    console.clear();
+    console.log(chalk.rgb(141, 11, 212).bold(`\t\tQuiz App\n\tDeveloped by Ayyan Ali Khan`));
+    setTimeout(() => {
+        main();
+    }, 2000);
+}
+welcome();
+async function main() {
+    let score = 0;
+    for (let i = 0; i < 10; i++) {
+        let info = data[i];
+        let question = info.question;
+        let answer = info.correct_answer;
+        let options = [...info.incorrect_answers, answer];
+        let response = await inquirer.prompt({
+            name: "ans",
+            message: question,
+            type: "list",
+            choices: options,
+        });
+        let result = response.ans;
+        if (result === answer) {
+            score += 1;
+            console.log(chalk.greenBright("Correct answer"));
+        }
+        else {
+            console.log(chalk.redBright("incorrect answer"));
+        }
+    }
+    console.log(chalk.blueBright.bold(`\tYour score is ${score}`));
+    let ans = await inquirer.prompt({
+        name: "response",
+        message: "Want to use again?",
+        type: "confirm",
+    });
+    decision = ans.response;
+    if (decision) {
+        welcome();
+    }
+    else {
+        console.log(chalk.redBright('Exiting'));
+    }
+}
